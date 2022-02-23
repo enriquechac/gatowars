088741f9740cb313b41c3a7f5d8a55518941ef51
Enrique Chacón <47252212+enriquechac@users.noreply.github.com>
2022-02-23 16:39:30 -0600

    Se agrego animacion al componente TableroGato cuando hay empate

--- src/components/TableroGato.jsx
+++ src/components/TableroGato.jsx
@@ -1,26 +1,80 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
-import styles from '../styles/tableroGato.module.css'
+import styles from '../styles/tableroGato.module.css';
+
+export const TableroGato = ({ turno = false, empate = false }) => {
+  const valor = [
+    ['😉', '🐧', '🐧'],
+    ['😉', '😉', '🐧'],
+    ['🐧', '😉', '😉'],
+	];
+	
+	const [animacionEmpate, setAnimacionEmpate] = useState([
+    [false, false, false],
+    [false, false, false],
+    [false, false, false],
+	])
+
+	useEffect(() => {
+
+		if (!empate) {
+			return
+		}
+
+		const ordenAnimation = {
+			0: {row:0, col:2},
+			1: {row:0, col:1},
+			2: {row:0, col:0},
+			3: {row:1, col:0},
+			4: {row:2, col:0},
+			5: {row:2, col:1},
+			6: {row:2, col:2},
+			7: {row:1, col:2},
+			8: {row:1, col:1},
+		}
 
 
-export const TableroGato = () => {
+		let avance = 0;
+		const interval = setInterval(() => {
+			if (avance < 9) {
+				const { row, col } = ordenAnimation[avance];
+				setAnimacionEmpate([...animacionEmpate, animacionEmpate[row][col] = true])
+				avance++
+			} else {
+				clearInterval(interval);
+			}
+    }, 150);
 
-    const valor = [
-        ['😾', 'x', 'x',],
-        ['x', 'x', 'x',],
-        ['x', 'x', 'x',],
-    ]
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
   return (
-      <div>
-          <h1>gato</h1>
-        <table className={styles.tablero}>
-            {valor.map(row => {
-                return (
-                    <tr>{row.map(valor => <td><p className={`${styles.text} + ${styles.last}`}>{valor}</p></td>)}</tr>
-                )
-            })}
-        </table>
+    <div>
+      <h1>gato</h1>
+      <table className={styles.tablero}>
+        <tbody>
+          {valor.map((row, indexRow) => {
+            return (
+              <tr key={`${indexRow}-tr`}>
+                {row.map((valor, indexCol) => (
+                  <td
+                    key={`${indexCol}-td`}
+                    className={` ${turno && styles.turno} ${ animacionEmpate[indexRow][indexCol] && styles.animacion} `}
+                  >
+                    <p
+                      className={`${styles.text} + ${styles.last} + ${styles.noselect}`}
+                    >
+                      {valor}
+                    </p>
+                  </td>
+                ))}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
